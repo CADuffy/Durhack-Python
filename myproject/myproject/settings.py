5abf9ed974ff18e1e0fa23aac2e1e58bc4431d5b
Jack Biddlecombe <jackbid@hotmail.co.uk>
2017-12-09 19:25:59 +0000

    Changed index

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'main',
+        'USER': 'CADurhack',
+        'PASSWORD': 'durhackpass',
+        'HOST': 'durhack2017.cqiorekc99vt.eu-west-2.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
