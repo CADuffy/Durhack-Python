c808803fa492a47a752a22ec14f279f26b28690f
Jack Biddlecombe <jackbid@hotmail.co.uk>
2017-12-09 20:09:08 +0000

    ID

--- myproject/queueManager/views.py
+++ myproject/queueManager/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    #text = "Enter ID:"
     return render(request, "index.html", {})
 
 def waiting(request):
-    text = "Waiting..."
-    return render(request, "waiting.html", {})
+    return render(request, "waiting.html", {"number": 12})
 
 def front(request):
-    text = "Front of queue"
-    return HttpResponse(text)
+    return render(request, "front.html", {})
