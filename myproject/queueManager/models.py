9c223048099b5d8efc7ee65d25a6921dbd1dbebd
Jack Biddlecombe <jackbid@hotmail.co.uk>
2017-12-09 23:24:25 +0000

    GOT SOMETHING TO GO INTO THE DATABASE!!

--- myproject/queueManager/models.py
+++ myproject/queueManager/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class PhoneNumber(models.Model):
     pos = models.IntegerField(default=0)
-    number0 = models.CharField(max_length=30)
-
+    numbers0 = models.CharField(max_length=30)
+    class Meta:
+        db_table = 'queues'
--- myproject/queueManager/views.py
+++ myproject/queueManager/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import models
 
+from .models import PhoneNumber
+
+
 def index(request):
     return render(request, "index.html", {})
 
 def waiting(request):
     inp_value = request.GET.get("id")
     # add inp_value to mysql database
-
+    entry = PhoneNumber(pos=5,numbers0=inp_value)
+    entry.save()
     return render(request, "waiting.html", {"number": inp_value})
 
 def front(request):
