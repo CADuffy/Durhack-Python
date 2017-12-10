d194f10d2e39e68e5df472f42b91dc089d23097b
Jack Biddlecombe <jackbid@hotmail.co.uk>
2017-12-10 01:10:59 +0000

    working version

--- myproject/queueManager/views.py
+++ myproject/queueManager/views.py
@@ -23,12 +23,11 @@ def front(request):
     return render(request, "front.html", {})
 
 def company(request):
+
     first = PhoneNumber.objects.raw("SELECT * FROM main.queues LIMIT 1")
     last = PhoneNumber.objects.raw("SELECT * FROM main.queues ORDER BY  pos DESC LIMIT 1")
 
     actualLast = last[0].pos - (first[0].pos -1)
 
-    inp_value = request.GET.get("next-person")
-    print(inp_value)
 
     return render(request, "company waiting.html", {"last": actualLast})
